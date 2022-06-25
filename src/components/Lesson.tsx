6619eb54b526910671c31e9ca0307f7ecf7922a3
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-06-25 11:33:48 -0300

    css adjusts

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,3 +1,22 @@
-export function Lessons() {
-  return <h1>Lessons</h1>;
+export function Lesson() {
+  return (
+    <a href="">
+      <span className="text-gray-300">Terça • 22 de junho • 19h00</span>
+
+      <div className="rounded border border-gray-500 p-4 mt-2">
+        <header className="flex items-center justify-between">
+          <span className="text-sm text-blue-500 font-medium">
+            Conteúdo Liberado
+          </span>
+          <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 ">
+            AO VIVO
+          </span>
+        </header>
+
+        <strong className="text-gray-200 mt-5 block">
+          Abertura do evento Ignite labs
+        </strong>
+      </div>
+    </a>
+  );
 }
