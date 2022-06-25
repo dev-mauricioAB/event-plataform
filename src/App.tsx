4e8da8beab971027db9a89da8d142044b0aac63d
MauricioAB <dev.mauricioalezandre@gmail.com>
2022-06-25 10:49:40 -0300

    clear code

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
 import { gql, useQuery } from "@apollo/client";
-import { useEffect } from "react";
-import { client } from "./lib/apolo";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`;
-
-interface LessonProps {
-  id: string;
-  title: string;
-}
 
 function App() {
-  const { data } = useQuery<{ lessons: LessonProps[] }>(GET_LESSONS_QUERY);
-
-  return (
-    <ul>
-      {data?.lessons.map((lesson) => {
-        return <li key={lesson.id}>{lesson.title}</li>;
-      })}
-    </ul>
-  );
+  return <h1>Opa</h1>;
 }
 
 export default App;
